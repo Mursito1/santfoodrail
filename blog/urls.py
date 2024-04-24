ae7aa8ab93e0cf3b506db44e7182dbd19e8a6d80
Mursito1 <deltamur@hotmail.com>
2024-04-24 13:50:49 -0400

    cambios en navbar

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.render_articles, name='index')
+    path('', views.render_articles, name='index'),
+    path('menus', views.menus, name = 'menus'),
+    path('registro', views.registro , name = 'registro'),
+    path('nosotros', views.nosotros, name = 'nosotros'),
+    path('contacto', views.contacto, name = 'contacto'),
+    path('login', views.login, name = 'login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def render_articles(request):
     return render(request, 'index.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def menus(request):
+    return render(request, 'menus.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def registro(request):
+    return render(request, 'registro.html')

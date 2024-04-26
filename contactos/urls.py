5e7905068944ccd3982976f52a12a66bbe267d32
Mursito1 <deltamur@hotmail.com>
2024-04-26 18:40:20 -0400

    migrations actualizado

--- contactos/urls.py
+++ contactos/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .views import ContactoViewSet
 
 router = routers.DefaultRouter()
-router.register(r'contactos', ContactoViewSet, basename='contactos')
+router.register(r'contacto', ContactoViewSet, basename='contacto')
 
 urlpatterns = router.urls

940650365c5c96fbffee42d771e1f2f0bd27046a
Mursito1 <deltamur@hotmail.com>
2024-05-15 22:26:31 -0400

    cambios en settings

--- santfood/settings.py
+++ santfood/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 from dotenv import load_dotenv
-import os
+
 
 load_dotenv()
 

64ea0e439263cf4b92f76844f36b72147c12bd7e
Mursito1 <deltamur@hotmail.com>
2024-06-14 18:22:44 -0400

    validacion de menus

--- menus/models.py
+++ menus/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, RegexValidator, MinLengthValidator
 
 
 # Create your models here.
@@ -34,11 +36,21 @@ class Salsa(models.Model):
         return self.nombre
 
 class Menu(models.Model):
-    nombre_menu = models.CharField(max_length=50)
-    precio = models.PositiveIntegerField()
-    ganancia = models.PositiveIntegerField()
+    nombre_menu = models.CharField(
+        max_length=50,
+        validators=[RegexValidator(
+            regex='^[a-zA-Z]+$',
+            message='El nombre del menú solo puede contener letras.',
+            code='invalid_nombre_menu'
+        )]
+    )
+    precio = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    ganancia = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     estado_menu = models.BooleanField()
-    descripcion_menu = models.CharField(max_length=300)
+    descripcion_menu = models.CharField(
+        max_length=300,
+        validators=[MinLengthValidator(10)]
+    )
     imagen = models.ImageField(upload_to='images/')
     proteina = models.ManyToManyField(Proteina)
     vegetal = models.ManyToManyField(Vegetal)
@@ -46,6 +58,14 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.nombre_menu
+
+    def clean(self):
+        super().clean()
+
+        if not self.descripcion_menu or len(self.descripcion_menu) < 10:
+            raise ValidationError({
+                'descripcion_menu': 'La descripción debe tener al menos 10 caracteres.'
+            })
     
 class Calificacion_Menu(models.Model):
     id_menu = models.ForeignKey(Menu, on_delete=models.PROTECT)
